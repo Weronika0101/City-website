862e68358eaceb385f3521d88854f7fb3bb9dc8e
Weronika0101 <weronikalos01@gmail.com>
2023-11-18 12:52:48 +0100

    zad2 ??

--- zad2.js
+++ zad2.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", function() {
     // Accessing collections
     var imagesCollection = document.images;
@@ -6,17 +5,37 @@ document.addEventListener("DOMContentLoaded", function() {
     var formsCollection = document.forms;
     var anchorsCollection = document.anchors;
 
-    // Log information about the collections
-    console.log("Images Collection:", imagesCollection);
-    console.log("Links Collection:", linksCollection);
-    console.log("Forms Collection:", formsCollection);
-    console.log("Anchors Collection:", anchorsCollection);
+    // Display information about the collections on the webpage
+    displayCollectionInfo("Images Collection:", imagesCollection);
+    displayCollectionInfo("Links Collection:", linksCollection);
+    displayCollectionInfo("Forms Collection:", formsCollection);
+    displayCollectionInfo("Anchors Collection:", anchorsCollection);
 
     // Demonstrating item and namedItem methods
     var firstImage = imagesCollection.item(0); // Get the first image
     var firstLink = linksCollection.namedItem("firstLink"); // Assuming you have an anchor with name="firstLink"
 
-    console.log("First Image:", firstImage);
-    console.log("Link with name 'firstLink':", firstLink);
-  
+    // Display information about the first image and link on the webpage
+    displayItemInfo("Link with name 'firstLink':", firstLink);
+    displayItemInfo("First Image:", firstImage);
+
+
+    function displayCollectionInfo(label, collection) {
+        // Create a paragraph element for collection information
+        var infoElement = document.createElement("p");
+        infoElement.textContent = label + " " + collection.length + " elements";
+        infoElement.style.marginBottom = "50px";
+        // Append the infoElement to the body
+        document.body.appendChild(infoElement);
+    }
+
+    function displayItemInfo(label, item) {
+        // Create a paragraph element for item information
+        var infoElement = document.createElement("p");
+        infoElement.textContent = label + " " + (item ? item.nodeName : "Not found");
+        
+        infoElement.style.marginBottom = "50px";
+        // Append the infoElement to the body
+        document.body.appendChild(infoElement);
+    }
 });
