2dfad39d42fc44cbafda162a474899c21d1ca5ab
Kacper Gaudyn <kacper.gaudyn@pwr.edu.pl>
2023-12-06 18:35:20 +0100

    ??

--- form.php
+++ form.php
@@ -1,65 +1,85 @@
 <?php
-// Definicja stałych
-define("MAX_WIADOMOSC_LENGTH", 200);
+// Sprawdzenie, czy formularz został wysłany
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Funkcja walidująca dane tekstowe
+    function validateText($input, $fieldName, $minLength, $maxLength) {
+        $input = trim($input);
 
-// Funkcja sprawdzająca poprawność numeru telefonu
-function validatePhone($phone) {
-    return preg_match("/^\+\d{2} \d{3}-\d{3}-\d{3}$/", $phone);
+        if (empty($input) || strlen($input) < $minLength || strlen($input) > $maxLength) {
+            return "Pole $fieldName powinno mieć od $minLength do $maxLength znaków.";
+        }
+
+        return "";
+    }
+
+    // Funkcja walidująca adres e-mail
+    function validateEmail($email) {
+        $email = trim($email);
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return "Proszę podać poprawny adres e-mail.";
+        }
+
+        return "";
+    }
+
+    // Funkcja walidująca numer telefonu
+    function validatePhone($phone) {
+        $phone = trim($phone);
+
+        if (!preg_match("/^\+\d{2} \d{3}-\d{3}-\d{3}$/", $phone)) {
+            return "Proszę podać poprawny numer telefonu.";
+        }
+
+        return "";
+    }
+
+    // Pobranie danych z formularza
+    $zaimek = $_POST["zaimek"];
+    $imie = $_POST["imie"];
+    $nazwisko = $_POST["nazwisko"];
+    $miesiac = $_POST["miesiac"];
+    $email = $_POST["email"];
+    $telefon = $_POST["telefon"];
+    $wiadomosc = $_POST["wiadomosc"];
+    $kategoria = $_POST["kategoria"];
+    $tematy = $_POST["temat"];
+
+    // Walidacja danych
+    $errors = array();
+    $fieldsToValidate = array(
+        array("imie", 2, 50),
+        array("nazwisko", 2, 50)
+        array("wiadomosc", 1, 200)
+    );
+
+    for ($i = 0; $i < count($fieldsToValidate); $i++) {
+        $fieldName = $fieldsToValidate[$i][0];
+        $input = $_POST[$fieldName];
+        $minLength = $fieldsToValidate[$i][1];
+        $maxLength = $fieldsToValidate[$i][2];
+        $errors[$fieldName] = validateText($input, $fieldName, $minLength, $maxLength);
+    }
+
+    // $errors["imie"] = validateText($imie, "Imię", 2, 50);
+    // $errors["nazwisko"] = validateText($nazwisko, "Nazwisko", 2, 50);
+    $errors["email"] = validateEmail($email);
+    $errors["telefon"] = validatePhone($telefon);
+    // $errors["wiadomosc"] = validateText($wiadomosc, "Wiadomość", 1, 200);
+    $errors["kategoria"] = empty($kategoria) ? "Proszę wybrać kategorię." : "";
+    $errors["tematy"] = empty($tematy) ? "Proszę wybrać przynajmniej jeden temat." : "";
+
+    // Wyświetlanie ewentualnych błędów
+    foreach ($errors as $error) {
+        if (!empty($error)) {
+            die($error);
+        }
+    }
+
+    // Jeśli dane są poprawne, można wykonywać dalsze operacje
+    // ...
+
+    // Przykładowa odpowiedź
+    echo "Dane zostały poprawnie przetworzone!";
 }
-
-// Pobranie danych z formularza
-$zaimek = $_POST['zaimek'];
-$imie = $_POST['imie'];
-$nazwisko = $_POST['nazwisko'];
-$miesiac = $_POST['miesiac'];
-$email = $_POST['email'];
-$telefon = $_POST['telefon'];
-$wiadomosc = $_POST['wiadomosc'];
-$kategoria = $_POST['kategoria'];
-$tematy = isset($_POST['temat']) ? $_POST['temat'] : [];
-$niedostepnePole = $_POST['niedostepny'];
-
-// Walidacja danych
-if (!in_array($zaimek, ['opcja1', 'opcja2', 'opcja3'])) {
-    die("Błędny zaimek");
-}
-
-if (empty($imie) || !is_string($imie)) {
-    die("Błędne imię");
-}
-
-if (empty($nazwisko) || !is_string($nazwisko)) {
-    die("Błędne nazwisko");
-}
-
-if (empty($miesiac) || !is_string($miesiac)) {
-    die("Błędny miesiąc urodzin");
-}
-
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    die("Błędny adres e-mail");
-}
-
-if (!validatePhone($telefon)) {
-    die("Błędny numer telefonu");
-}
-
-if (strlen($wiadomosc) > MAX_WIADOMOSC_LENGTH) {
-    die("Wiadomość przekracza maksymalną długość");
-}
-
-if (!in_array($kategoria, ['pytanie', 'uwaga', 'inna'])) {
-    die("Błędna kategoria");
-}
-
-// Przetwarzanie tematów
-foreach ($tematy as $temat) {
-    // Tutaj można dodatkowo sprawdzić, czy wartość tematu jest poprawna
-}
-
-// Przykład użycia tablic superglobalnych
-echo "Adres IP klienta: " . $_SERVER['REMOTE_ADDR'];
-
-// Tutaj możesz dodać kod odpowiedzialny za dalsze przetwarzanie danych
-
 ?>
