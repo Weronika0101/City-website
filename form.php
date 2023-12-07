879e2af410bc6e8c7abe9856675074b57eaa5840
Kacper Gaudyn <kacper.gaudyn@pwr.edu.pl>
2023-12-07 12:12:50 +0100

    php_1 done

--- form.php
+++ form.php
@@ -11,9 +11,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     function validateEmail($email) {
-        $email = trim($email);
+        $trimmed_email = trim($email);
 
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        if (!filter_var($trimmed_email, FILTER_VALIDATE_EMAIL)) {
             return "Proszę podać poprawny adres e-mail.";
         }
 
@@ -30,21 +30,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         return "";
     }
 
-    $zaimek = $_POST["zaimek"];
-    $imie = $_POST["imie"];
-    $nazwisko = $_POST["nazwisko"];
-    $miesiac = $_POST["miesiac"];
-    $email = $_POST["email"];
-    $telefon = $_POST["telefon"];
-    $wiadomosc = $_POST["wiadomosc"];
-    $kategoria = $_POST["kategoria"];
-    $tematy = $_POST["temat"];
-
     $errors = array();
     $fieldsToValidate = array(
         array("imie", 2, 50),
         array("nazwisko", 2, 50),
-        array("wiadomosc", 1, 200)
+        array("wiadomosc", 10, 200)
     );
 
     for ($i = 0; $i < count($fieldsToValidate); $i++) {
@@ -55,10 +45,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $errors[$fieldName] = validateText($input, $fieldName, $minLength, $maxLength);
     }
 
-    $errors["email"] = validateEmail($email);
-    $errors["telefon"] = validatePhone($telefon);
-    $errors["kategoria"] = empty($kategoria) ? "Proszę wybrać kategorię." : "";
-    $errors["tematy"] = empty($tematy) ? "Proszę wybrać przynajmniej jeden temat." : "";
+    define("EMAIL", $_POST["email"]);
+    define("TELEFON", $_POST["telefon"]);
+    define("KATEGORIA", $_POST["kategoria"]);
+    define("TEMATY", $_POST["temat"]);
+
+    $errors["email"] = validateEmail(EMAIL);
+    $errors["telefon"] = validatePhone(TELEFON);
+    $errors["kategoria"] = empty(KATEGORIA) ? "Proszę wybrać kategorię." : "";
+    $errors["tematy"] = empty(TEMATY) ? "Proszę wybrać przynajmniej jeden temat." : "";
 
     foreach ($errors as $error) {
         if (!empty($error)) {
@@ -66,9 +61,51 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     }
 
-    // Jeśli dane są poprawne, można wykonywać dalsze operacje
-    // ...
 
-    echo "Dane zostały poprawnie przetworzone!";
+    echo "Dane zostały poprawnie przetworzone na adresie {$_SERVER['REMOTE_ADDR']}";
+
+    $assocArray = array(
+        "1" => "1.1", 
+        "2" => "2.1", 
+        "3" => "3.1", 
+        "4" => "4.1"
+    );
+    $sum_int = 0;
+    $sum_float = 0.0;
+    echo "<br><br>TABLICA: 1=>1.1, 2=>2,1, 3=>3.1 4=>4.1";
+    echo "<br>Obecny element: ", key($assocArray), " => ", current($assocArray); 
+    $sum_int += (int)key($assocArray);
+    $sum_float += (float)current($assocArray);
+    next($assocArray);
+    echo "<br>Następny element: ", key($assocArray), " => ", current($assocArray); 
+    $sum_int += (int)key($assocArray);
+    $sum_float += (float)current($assocArray);
+    next($assocArray);
+    echo "<br>Następny element: ", key($assocArray), " => ", current($assocArray);
+    $sum_int += (int)key($assocArray);
+    $sum_float += (float)current($assocArray);
+    reset($assocArray); 
+    echo "<br>Po uzyciu reset: ", key($assocArray), " => ", current($assocArray); 
+    $sum_int += (int)key($assocArray);
+    $sum_float += (float)current($assocArray);
+
+    echo "<br><br>Suma kluczy podczas iteracji (s1): ", $sum_int;
+    echo "<br>Suma wartości podczas iteracji (s2): ", $sum_float;
+    echo "<br>Czy s1*s2+10 = 10+s1*s2?:  ", $sum_int * $sum_float + 10 == 10 + $sum_int * $sum_float;
+
+    $pattern = '/(\+\d{2} \d{3})-(\d{3})-(\d{3})/';
+    $replacement = '$1 $2 $3';
+    $newPhoneNumber = preg_replace($pattern, $replacement, $_POST["telefon"]);
+    echo "<br><br>Pobrany nr tel: ", $_POST['telefon'];
+    echo "<br>Po uyciu preg_replace: ", $newPhoneNumber;
+    echo "<br>Czy te numery są takie same?: ", (int) ($_POST['telefon'] == $newPhoneNumber);
+
+    define('S1', 'abc');
+    define('S2', 'abcd');
+    $compare = strcmp(S1, S2);
+    echo "<br><br>S1: abc, S2: abcd";
+    echo "<br>Czy\040S1\040jest\040dłusze\040od\040S2:\040", (int) ($compare > 0);
+    echo "<br>Czy\040S1\040i\040S2\040są\040równe:\040", (int) ($compare == 0);
+    echo "<br>Czy\040S1\040jest\040krótsze\040od\040S2:\040", (int) ($compare < 0);
 }
 ?>
